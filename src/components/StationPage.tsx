0b4e93f0aff64586714f12374be7323599720ef8
Daniel Miller <dan.cm@traditionasia.com>
2024-12-13 16:44:28 +0700

    some UI changes

--- src/components/StationPage.tsx
+++ src/components/StationPage.tsx
@@ -1,5 +1,8 @@
-import React, { useMemo } from "react"
+import React, { useMemo, useState } from "react"
 import { Station } from "../models/user"
+import { Button, Col, Row, Select, Space } from "antd"
+import { useQuery } from "@tanstack/react-query"
+import { StationService } from "../services/api"
 
 type props = {
     data: Station[],
@@ -10,7 +13,30 @@ export const StationPage: React.FC<props> = ({data, setEnabled}) => {
     const stations = useMemo(() => {
             return data.map((x) => x.dustboy_name_en);
         }, [data])
+
+    const [selectedStation, setSelectedStation] = useState<string | null>(null);
+    
+    const onSelect = (v: string) => {
+        setSelectedStation(v)
+        }   
     return (
-        <div>{stations}</div>
+        <Row style={{margin: 8}}>
+            <Space direction="vertical">
+                <Row>
+                    {/* <Button onClick={() => setEnabled(!isEnabled)}>Toggle to {isEnabled ? `Disable` : `Enable`}</Button> */}
+                </Row>
+                <Row>
+                    <Col span={24}>
+                        <Select style={{width: 300}} onSelect={onSelect}>
+                            {stations.map((x) => (
+                                <option key={x} value={x}>
+                                    {x}
+                                </option>
+                            ))}
+                        </Select>
+                    </Col>
+                </Row>
+                </Space>
+                </Row>
     )
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,24 @@
-import { Station } from "../models/user";
+import { Station, StationValue } from "../models/user";
 import apiClient from "./api/http-common";
+
+export const StationService = {
+  // Get all stations
+  getAll: async () => {
+    const response = await apiClient.get<Station[]>('/stations');
+    return response.data;
+  },
+  // Get specific station details
+  getStationById: async (id: number) => {
+    const response = await apiClient.get<Station>(`/station/${id}`);
+    return response.data
+  },
+
+  // Get air quality values for a specific device
+  getStationValues: async (dustboyId: string) => {
+    const response = await apiClient.get<StationValue>(`/value/${dustboyId}`);
+    return response.data;
+  },
+}
 
 export const getAll = async (): Promise<Station[]> => {
   const res = await apiClient.get("/stations");
